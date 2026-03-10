8bcb81c935bedd6655b3e2ea3d08d00fb600dc60
AaronFrancis05 <aarontaremwa8@gmail.com>
2026-03-10 10:38:30 +0300

    First commit

---
+++ src/Layout.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Layout {
+    JFrame Border;
+    public Layout(){
+        this.CreateBorder();
+    }
+    public JFrame CreateBorder(){
+         Border = new JFrame("This is a border Frame");
+        Border.setSize(1000,500);
+
+        Border.setVisible(true);
+        return Border;
+    }
+
+}
