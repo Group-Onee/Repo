d1280db2344aa149d69d759e9b4cc67f289a8cbc
AaronFrancis <aarontaremwa8@gmail.com>
2026-03-25 22:58:25 +0300

    Add testing

---
+++ src/test/java/test.java
@@ -0,0 +1,12 @@
+package test.java;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class test {
+
+    @Test
+    public void testAddition() {
+        assertEquals(2, 1 + 1);
+    }
+}
